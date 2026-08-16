2bac3e2524861deab826d228582723be05fc0b89
MoRsHeD <mdmorshed0187@gmail.com>
2026-08-16 11:37:16 +0600

    trasnfered static content of not found and error pages to EN and translated to PT and updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: "DeliGo Rider Registration Form | Check Status",
   description:
     "DeliGo Rider Registration Form enables one to register as a Delivery Partner if there is no Fleet Manager in his area.",
+  other: {
+    google: "notranslate",
+  },
 };
 
 export default function RootLayout({
@@ -16,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" translate="no">
+    <html lang="en" translate="no" className="notranslate">
       <body className={`antialiased bg-gray-50`}>
         {children}
         <Toaster richColors position="top-center" />
